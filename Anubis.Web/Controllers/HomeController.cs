e4f5aa31db3e4cf81aa3dd835b8668d2739421a7
Tomas McGuinness <tomas@chewedpencil.net>
2013-02-26 15:33:17 +0000

    Added some basic information to the website.

--- Anubis.Web/Controllers/HomeController.cs
+++ Anubis.Web/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,22 +13,35 @@ namespace Anubis.Web.Controllers
   {
     public ActionResult Index()
     {
-      ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+      return View();
+    }
+
+    public ActionResult Signup()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Signup(string emailAddress)
+    {
+      if (string.IsNullOrEmpty(emailAddress))
+      {
+        ModelState.AddModelError("emailAddress", "Required");
+      }
+
+      if (ModelState.IsValid)
+      {
+        var body = new StringContent(string.Format("{{ \"email\": \"{0}\"}}", emailAddress), Encoding.UTF8, "application/json");
+        HttpClient client = new HttpClient();
+        await client.PostAsync("https://passverse.prefinery.com/api/v2/betas/3654/testers?api_key=Cq78siCqzM777fUfc11s", body);
+        return RedirectToAction("Complete");
+      }
 
       return View();
     }
 
-    public ActionResult About()
+    public ActionResult Complete()
     {
-      ViewBag.Message = "Your app description page.";
-
-      return View();
-    }
-
-    public ActionResult Contact()
-    {
-      ViewBag.Message = "Your contact page.";
-
       return View();
     }
   }
