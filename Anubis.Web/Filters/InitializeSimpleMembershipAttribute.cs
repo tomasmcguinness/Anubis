7385d618266149d3a2b0fdbdbb3d02a93601fc33
Tomas McGuinness <tomas@tomasmcguinness.com>
2013-02-28 09:06:29 +0000

    Cleaned up Account views. Added basic Dashboard

--- Anubis.Web/Filters/InitializeSimpleMembershipAttribute.cs
+++ Anubis.Web/Filters/InitializeSimpleMembershipAttribute.cs
@@ -38,7 +38,7 @@ namespace Anubis.Web.Filters
             }
           }
 
-          WebSecurity.InitializeDatabaseConnection("DefaultConnection", "UserProfile", "UserId", "UserName", autoCreateTables: true);
+          WebSecurity.InitializeDatabaseConnection("AnubisContext", "UserProfile", "UserId", "UserName", autoCreateTables: true);
         }
         catch (Exception ex)
         {
